d572a909b77c6ca052c6c3e167f849d559950729
Sam Singer <samuel.singer1@gmail.com>
2018-10-20 22:08:20 -0700

    background image set along with spacing

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -10,8 +10,8 @@ const card = (props) => {
             </h2>
             <h5><strong>GENDER: </strong>{props.gender}</h5>
             <h5><strong>HAIR: </strong>{props.hair}</h5>
-            <h5><strong>HEIGHT: </strong>{props.height}</h5>
-            <h5><strong>MASS: </strong>{props.mass}</h5>
+            <h5><strong>HEIGHT: </strong>{props.height} cm</h5>
+            <h5><strong>MASS: </strong>{props.mass} kg</h5>
         </div>
     )
 }
